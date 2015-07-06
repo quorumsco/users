cdacf87380e63a3843b9433471daa215b598650d
Douézan-Grard Guillaume <silverwyrda@gmail.com>
2015-07-06 15:06:04 +0200

    Add vars

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"runtime"
 
 	"github.com/codegangsta/cli"
@@ -26,8 +27,20 @@ func main() {
 	cmd.Version = "0.0.1"
 	cmd.Before = serve
 	cmd.Flags = append(cmd.Flags, []cli.Flag{
-		cli.BoolFlag{Name: "migrate, m", Usage: "migrate the database"},
-		cli.StringFlag{Name: "listen, l", Value: "0.0.0.0:8080", Usage: "server listening host:port"},
+		cli.StringFlag{Name: "listen-host", Value: "0.0.0.0", Usage: "server listening host", EnvVar: "LISTEN_HOST"},
+		cli.IntFlag{Name: "listen-port", Value: 8080, Usage: "server listening port", EnvVar: "LISTEN_PORT"},
+
+		cli.StringFlag{Name: "sql-dialect", Value: "postgres", Usage: "database dialect ('sqlite' or 'postgres')", EnvVar: "SQL_DIALECT"},
+
+		cli.StringFlag{Name: "postgres-host", Value: "postgres", Usage: "postgresql host", EnvVar: "POSTGRES_HOST"},
+		cli.IntFlag{Name: "postgres-port", Value: 5432, Usage: "postgresql port", EnvVar: "POSTGRES_PORT"},
+		cli.StringFlag{Name: "postgres-user", Value: "postgres", Usage: "postgresql user", EnvVar: "POSTGRES_USER"},
+		cli.StringFlag{Name: "postgres-password", Value: "postgres", Usage: "postgresql password", EnvVar: "POSTGRES_PASSWORD"},
+		cli.StringFlag{Name: "postgres-db", Value: "postgres", Usage: "postgresql database", EnvVar: "POSTGRES_DB"},
+
+		cli.StringFlag{Name: "sqlite-path", Value: "/tmp/db.sqlite", Usage: "sqlite path", EnvVar: "SQLITE_PATH"},
+
+		cli.BoolFlag{Name: "migrate, m", Usage: "migrate the database", EnvVar: "MIGRATE"},
 		cli.BoolFlag{Name: "debug, d", Usage: "print debug information", EnvVar: "DEBUG"},
 		cli.HelpFlag,
 	}...)
@@ -42,13 +55,21 @@ func serve(ctx *cli.Context) error {
 		logs.Level(logs.DebugLevel)
 	}
 
+	postgres := fmt.Sprintf("postgres://%s:%s@%s:%d/%s?sslmode=verify-full",
+		ctx.String("postgres-user"),
+		ctx.String("postgres-password"),
+		ctx.String("postgres-host"),
+		ctx.Int("postgres-port"),
+		ctx.String("postgres-db"),
+	)
+
 	if ctx.Bool("migrate") {
-		migrate()
+		migrate("postgres", postgres)
 		logs.Debug("Database migrated")
 	}
 
 	app = application.New()
-	if app.Components["DB"], err = sqlx.Connect("sqlite3", "/tmp/contacts.db"); err != nil {
+	if app.Components["DB"], err = sqlx.Connect("postgres", postgres); err != nil {
 		return err
 	}
 	app.Components["Templates"] = views.Templates()
@@ -63,8 +84,8 @@ func serve(ctx *cli.Context) error {
 	return app.Serve(ctx.String("listen"))
 }
 
-func migrate() {
-	db, err := gorm.Open("sqlite3", "/tmp/contacts.db")
+func migrate(dialect string, args string) {
+	db, err := gorm.Open(dialect, args)
 	if err != nil {
 		logs.Error(err)
 		return
